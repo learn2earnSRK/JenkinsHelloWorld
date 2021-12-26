bafa0c5528a1ec780184f60d44522c03c333fd5c
learn2earnSRK <srikanth.sama678@gmail.com>
2021-12-26 08:18:00 +0000

    edit5

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
 	public static void main(String[] args){
-		System.out.println("-----***** ---Learning Devops-Jenkins--- *****-----");
+		System.out.println("-----***** ---Learning GitHubHookPollSCM--- *****-----");
 	}
 }
